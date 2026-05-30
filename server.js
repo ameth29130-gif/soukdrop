cd2a0ed4023aa8929782eb3d5fb05be245361fd7
ameth29130-gif <ameth29.130@gmail.com>
2026-05-30 02:48:39 +0000

    Fix: Restructuration des accolades de fermeture server.js

--- server.js
+++ server.js
@@ -605,6 +605,8 @@ app.get('/api/health', async (req, res) => {
   res.json({ status: 'ok', supabase: !error ? 'connected' : 'error', cj: !!(process.env.CJ_EMAIL), paytech: !!(process.env.PAYTECH_API_KEY), resend: !!(process.env.RESEND_API_KEY), ts: new Date().toISOString() });
 });
 
+}
+
 // -- Frontend ------------------------------------------------------
 app.use(express.static(__dirname));
 app.get('/{*path}', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
@@ -617,6 +619,7 @@ app.listen(PORT, () => {
   console.log('+------------------------------------------+');
   getCJToken().then(t => console.log(t ? '? CJ Connecté' : '? CJ: échec'));
 });
+
 
 
 
